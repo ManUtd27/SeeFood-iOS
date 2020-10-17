e338a0c6bdd5ec509b4e297f1cefc347f9618a3c
Shawn Williams <jr.william09@gmail.com>
2020-10-17 16:46:48 -0500

    setting up view controller for the image picker

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
+    
+    
+    /// Handles when the camera is tapped
+    /// - Parameter sender: The Nav bar item
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+}
 
-
+//MARK: - Delegate methods for Image Picker and Navigation Controller
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
 }
 
