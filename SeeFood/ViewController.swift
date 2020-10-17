e01a1a61871a1a8fba6f86f751e4539c0137d4b0
Shawn Williams <jr.william09@gmail.com>
2020-10-17 17:20:24 -0500

    workning on image picker customization

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -14,11 +14,18 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var imageView: UIImageView!
     
+    // Get th image picker object
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        // set the image picker delegate as self
+        imagePicker.delegate = self
+        // set the image picker source type
+        imagePicker.sourceType = .camera
+        // Set the image picker allows editting propery
+        imagePicker.allowsEditing = false
         
     }
     
@@ -26,12 +33,33 @@ class ViewController: UIViewController {
     /// Handles when the camera is tapped
     /// - Parameter sender: The Nav bar item
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        // Presents the impage picker object when the camera button is pressed
+        present(imagePicker, animated: true, completion: nil)
     }
 }
 
-//MARK: - Delegate methods for Image Picker and Navigation Controller
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//MARK: - Delegate methods for Image Picker
+extension ViewController: UIImagePickerControllerDelegate {
     
+    
+    /// Handles the logic when the user did finish picking a image or media with info
+    /// - Parameters:
+    ///   - picker: The current UI Picker
+    ///   - info: Info about the image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Get the image that user picked from the image picker
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            // Set that picked image as the image in the imageView
+            imageView.image = userPickedImage
+        }
+        // Dismiss the image picker after the selection
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+//MARK: - Delegat methods for Navigation Controller
+extension ViewController: UINavigationControllerDelegate {
     
 }
 
